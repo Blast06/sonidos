01d6e453bcb1b87d62cac9cee305b77fe43b0c72
Blast06 <lacalculadora06@hotmail.com>
2018-05-09 23:23:37 -0400

    app de sonidos terminada

---
+++ src/interfaces/animal.interface.ts
@@ -0,0 +1,9 @@
+export interface Animal {
+    nombre:string;
+    imagen:string;
+    audio:string;
+    duracion:number;
+    reproduciendo:boolean;
+}
+
+//ES PARA TENER UN MEJOR CONTROL DE LOS DATOS QUE ESTAREMOS LIDIANDO
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,87 @@
+import { Animal } from './../../interfaces/animal.interface';
+import { ANIMALES } from './../../data/data.animales';
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Refresher, reorderArray } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  animales: Animal[] = [];
+  audio = new Audio();
+  audioTiempo: number;
+  ordenando:boolean = false;
 
-  constructor(public navCtrl: NavController) {
+  constructor() {
+    this.animales = ANIMALES.slice(0); //crea un clon del objeto animales
+
+
 
   }
+
+  reproducir(animal: Animal) {
+    this.pausar_audio(animal);
+
+    if (animal.reproduciendo) {
+      animal.reproduciendo = false;
+      return;
+
+    }
+
+    this.audio.src = animal.audio;
+
+    this.audio.load();
+    this.audio.play();
+
+
+    animal.reproduciendo = true;
+
+    this.audioTiempo = setTimeout(() => animal.reproduciendo = false, animal.duracion * 1000);
+
+  }
+
+  private pausar_audio(animalSel: Animal) {
+    clearTimeout(this.audioTiempo)
+
+    this.audio.pause();
+    this.audio.currentTime = 0;
+
+
+    // esto es para poner en falso todos los animales que se estarian reproduiendo pero que no son 
+    // el que el usuario selecciono
+
+    for (let animal of this.animales) {
+      if (animal.nombre != animalSel.nombre) {
+        animal.reproduciendo = false;
+
+      }
+
+    }
+
+
+  }
+  borrar_animal(idx: number) {
+    this.animales.splice(idx,1);
+
+  }
+
+  recargar_animales(refresher:Refresher){
+    console.log("inicio del refresh");
+
+    setTimeout(() => {
+      console.log("termino el refresh");
+
+      this.animales = ANIMALES.slice(0);
+      refresher.complete();
+    }, 1500);
+  }
+
+  reordenar_animales( indices:any){
+
+    //esto es para reordenar la lista cuando se selecciona un item y se mueve a X sitio
+    this.animales = reorderArray(this.animales, indices);
+  }
+
 
 }
